73f595ae45c55fbc8d7d6b71885209bf44d3fab4
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-05-14 00:47:22 -0300

    ADD.: Adding confirmation screen

--- plantmanager/App.tsx
+++ plantmanager/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AppLoading from "expo-app-loading";
 // import { Welcome } from "./src/pages/Welcome";
 import { UserIdentification } from "./src/pages/UserIdentification";
+import { Confirmation } from "./src/pages/Confirmation";
 
 import {
   useFonts,
@@ -18,5 +19,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-  return <UserIdentification />;
+  return <Confirmation />;
 }
