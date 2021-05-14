dd5a662799a5b4e973f545d124b2571c2318b207
Samuel Alves Chagas <samuelchagasalves@gmail.com>
2021-05-14 00:32:08 -0300

    ADD.: Improving UX

--- plantmanager/src/pages/UserIdentification.tsx
+++ plantmanager/src/pages/UserIdentification.tsx
@@ -1,28 +1,67 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, Text, View, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { Button } from "../components/Button";
 import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.content}>
-        <View style={styles.form}>
-          <Text style={styles.emoji}>😄</Text>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.content}>
+          <View style={styles.form}>
+            <View style={styles.header}>
+              <Text style={styles.emoji}>{isFilled ? "😄" : "😀"}</Text>
 
-          <Text style={styles.title}>Como podemos{"\n"}chamar você?</Text>
+              <Text style={styles.title}>Como podemos{"\n"}chamar você?</Text>
+            </View>
 
-          <TextInput
-            style={styles.input}
-            placeholder='Digite um nome'
-          ></TextInput>
+            <TextInput
+              style={[
+                styles.input,
+                (isFocused || isFilled) && { borderColor: colors.green },
+              ]}
+              placeholder='Digite um nome'
+              onBlur={handleInputBlur}
+              onFocus={handleInputFocus}
+              onChangeText={handleInputChange}
+            ></TextInput>
 
-          <View style={styles.footer}>
-            <Button></Button>
+            <View style={styles.footer}>
+              <Button></Button>
+            </View>
           </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -43,6 +82,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     paddingHorizontal: 54,
+  },
+  header: {
+    alignItems: "center",
   },
   emoji: {
     fontSize: 44,
